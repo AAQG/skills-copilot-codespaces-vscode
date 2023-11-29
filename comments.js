7bd85d4e0cc575e395c602081619ad0546ab9cca
Alex Quintanilla <94883738+AAQG@users.noreply.github.com>
2023-11-29 15:33:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,34 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
+//Create Web Server
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+//Connect to MongoDB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/MyDatabase');
+//Create Schema
+const commentSchema = new mongoose.Schema({
+    id: String,
+    name: String,
+    comment: String
 });
-
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+//Create Model
+const Comment = mongoose.model('Comment', commentSchema);
+//Create Parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+//Create Router
+app.use(express.static('public'));
+app.get('/comment', (req, res) => {
+    Comment.find({}, (err, comments) => {
+        if (err) throw err;
+        res.render('index.ejs', { comments: comments });
+    });
 });
+app.post('/comment', urlencodedParser, (req, res) => {
+    let newComment = new Comment(req.body);
+    newComment.save((err) => {
+        if (err) throw err;
+        res.redirect('/comment');
+    });
+});
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
